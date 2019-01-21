c441b698aad14c476f15c21412aa5add4e15f7bb
Tony <yanjunjiangjn@gmail.com>
2019-01-20 21:19:36 -0500

    Two more dictionary examples.
    del key:value pair from dict
    Dict of similar objects.

---
+++ chapter6/favorite_languages.py
@@ -0,0 +1,9 @@
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edvard': 'ruby',
+    'phil': 'python',
+    }
+
+print("Sarah's favorite language is " + 
+        favorite_languages['sarah'].title() + ".")
