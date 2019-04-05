2bc33c3dce2e67818d0394c1b148b90a390fa276
Tony <yanjunjiangjn@gmail.com>
2019-04-04 23:01:21 -0400

    [Chapter9] Exercise 9.1 ~ 9.3.

---
+++ chapter9/restaurant_9_1.py
@@ -0,0 +1,18 @@
+class Restaurant():
+    """A simply attempt to model a restaurant."""
+
+    def __init__(self, restaurant_name, cuisine_type):
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+
+    def describe_restaurant(self):
+        print("The name of the restaurant is " + self.restaurant_name.title() + ".")
+        print("The cuisine type of the restaurant is " + self.cuisine_type.title() + ".")
+
+    def open_restaurant(self):
+        print("The restaurant " + self.restaurant_name.title() + " is now open!!")
+
+
+my_restaurant = Restaurant("Old Code Seller", "Chinese Canadian")
+my_restaurant.describe_restaurant()
+my_restaurant.open_restaurant()
---
+++ chapter9/user_9_3.py
@@ -0,0 +1,20 @@
+class User():
+    """A simply attempt to model a user."""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def describe_user(self):
+        print("The first name of the user is " + self.first_name.title() + ".")
+        print("The last name of the user is " + self.last_name.title() + ".")
+        print("The user's age is " + self.age + ".")
+
+    def greet_user(self):
+        print("Hello " + self.first_name.title() + " " + self.last_name.title()) 
+
+
+user1 = User("Wukong", "Sun", "18000")
+user1.describe_user()
+user1.greet_user()
