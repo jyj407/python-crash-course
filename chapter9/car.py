837ab996d96d9c0651a2b9e08545813452fecc1b
Tony <yanjunjiangjn@gmail.com>
2019-04-19 11:20:11 -0400

    [Chapter9] Storing Multiple Classes in a Module

--- chapter9/car.py
+++ chapter9/car.py
@@ -32,14 +32,30 @@ class Car():
         """Add the given amount to the odometer reading."""
         self.odometer_reading += miles
 
-my_new_car = Car('audi', 'a4', 2016)
-print(my_new_car.get_descriptive_name())
-my_new_car.update_odometer(23)
-my_new_car.read_odometer()
-my_new_car.update_odometer(22)
-my_new_car.read_odometer()
-my_new_car.update_odometer(24089)
-my_new_car.read_odometer()
-my_new_car.increment_odometer(10000)
-my_new_car.read_odometer()
+class Battery():
+    """A simple attempt to model a battery for an electric car."""
+    def __init__(self, battery_size=70):
+        """Initialize the batteery's attributes."""
+        self.battery_size = battery_size
+    def describe_battery(self):
+        """Print a statement describing the battery size."""
+        print("This car has a " + str(self.battery_size) + "-kWh battery.")
+    def get_range(self):
+        """Print a statement about the range this battery provides."""
+        if self.battery_size == 70:
+            range = 240
+        elif battery_size == 85:
+            range = 270
+        message = "This car can go approximately " + str(range)
+        message += " miles on a full charge."
+        print(message)
 
+class ElectricCar(Car):
+    """Models aspects of a car, specific to electric vehicles."""
+    def __init__(self, make, model, year):
+        """
+        Initialize attributes of the parent class.
+        Then initialize attributes specific to an electric car.
+        """
+        super().__init__(make, model, year)
+        self.battery = Battery()
