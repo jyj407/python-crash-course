267376c718505e9f0a02223188c9666749964c35
Tony <yanjunjiangjn@gmail.com>
2019-04-19 13:32:23 -0400

    [Chapter9] Exercise 9_10 imported restaurant.

---
+++ chapter9/imported_restaurant_9_10.py
@@ -0,0 +1,13 @@
+from restaurant import Restaurant
+
+my_restaurant = Restaurant("Old Code Seller", "Chinese Canadian")
+my_restaurant.describe_restaurant()
+my_restaurant.open_restaurant()
+
+your_restaurant = Restaurant("Dosa Haka", "Indian")
+your_restaurant.describe_restaurant()
+your_restaurant.open_restaurant()
+
+her_restaurant = Restaurant("Shawarma Stop", "Middle East")
+her_restaurant.describe_restaurant()
+her_restaurant.open_restaurant()
