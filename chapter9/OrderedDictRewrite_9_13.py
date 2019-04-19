eab87be4aaa9f70405b2dd0a30848e01c8774e72
Tony <yanjunjiangjn@gmail.com>
2019-04-19 14:00:51 -0400

    [Chapter9] Exercise 9-13 Use OrderedDict

---
+++ chapter9/OrderedDictRewrite_9_13.py
@@ -0,0 +1,8 @@
+from collections import OrderedDict
+
+glossary = OrderedDict()
+glossary = {'List': 'Array in C++', 'Dictionary': 'Map in C++', 'For': 'For in C++'}
+print("List:" + glossary['List'])
+print("Dictionary:" + glossary['Dictionary'])
+print("For:" + glossary['For'])
+
