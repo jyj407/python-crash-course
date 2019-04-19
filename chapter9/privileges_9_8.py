344dccf256d3234de12fb69ce79b6a3f1e5d7e3b
Tony <yanjunjiangjn@gmail.com>
2019-04-19 09:52:45 -0400

    [Chapter9] Privileges version 1.
    Just decouple the privileges from users.
    May not be the best solution.

---
+++ chapter9/privileges_9_8.py
@@ -0,0 +1,44 @@
+class User():
+    """A simply attempt to model a user."""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def describe_user(self):
+        print("The first name of the user is " + self.first_name.title() + ".")
+        print("The last name of the user is " + self.last_name.title() + ".")
+        print("The user's age is " + self.age + ".")
+
+    def greet_user(self):
+        print("Hello " + self.first_name.title() + " " + self.last_name.title()) 
+
+class Admin(User):
+    """A simply attempt to model an admin user."""
+
+    def __init__(self, first_name, last_name, age):
+        super().__init__(first_name, last_name, age)
+
+class Privileges:
+    """A simply attempt to model the privileges an admin user can have."""
+    
+    def __init__(self, privileges):
+        self.privileges = privileges
+
+    def show_privileges(self):
+        print("Here are the Admin user privileges");
+        for privilege in self.privileges:
+            print(privilege)
+
+
+user1 = User("Wukong", "Sun", "18000")
+user1.describe_user()
+user1.greet_user()
+
+privileges = ["Can add post", "Can delete post", "Can ban user"]
+admin_privileges = Privileges(privileges)
+user2 = Admin("Mouni", "Sijia", "36000000")
+user2.describe_user()
+user2.greet_user()
+admin_privileges.show_privileges()
