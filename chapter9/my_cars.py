d25e0650c2326de67af84e963e4a73cdb8a4a836
Tony <yanjunjiangjn@gmail.com>
2019-04-19 11:23:05 -0400

    [Chapter9] Importing Multiple Classes from a Module Example.

---
+++ chapter9/my_cars.py
@@ -0,0 +1,10 @@
+from car import Car, ElectricCar
+
+my_bettle = Car('volkswagen', 'bettle', 2016)
+print(my_bettle.get_descriptive_name())
+
+my_tesla = ElectricCar('telsa', 'roadster', 2016)
+print(my_tesla.get_descriptive_name())
+
+
+
