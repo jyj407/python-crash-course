84558a00e71a22747629c041957e89a8fad870e1
Tony <yanjunjiangjn@gmail.com>
2019-04-21 08:42:50 -0400

    [Chapter10] read line of a txt file.

--- chapter10/file_reader.py
+++ chapter10/file_reader.py
@@ -1,3 +1,4 @@
-with open('path/pi_digits.txt') as file_object:
-    contents = file_object.read()
-    print(contents.rstrip())
+filename = 'path/pi_digits.txt'
+with open(filename) as file_object:
+    for line in file_object:
+        print(line)
