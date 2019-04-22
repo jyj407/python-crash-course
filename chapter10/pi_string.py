fa5c43f2cb03f9ab2b3ff89c0c54fcfe096549f5
Tony <yanjunjiangjn@gmail.com>
2019-04-21 21:55:21 -0400

    [chapter10] Print millions digits of PI.

--- chapter10/pi_string.py
+++ chapter10/pi_string.py
@@ -1,4 +1,5 @@
-filename = 'path/pi_digits.txt'
+# filename = 'path/pi_digits.txt'
+filename = 'path/pi_million_digits.txt'
 with open(filename) as file_object:
     lines = file_object.readlines()
 
@@ -7,5 +8,11 @@ for line in lines:
     # pi_string += line.rstrip() 
     pi_string += line.strip()
 
-print(pi_string)
+print(pi_string[:52] + "...")
 print(len(pi_string))
+
+birthday = input("Enter your birthday, in the form mmddyy: ")
+if birthday in pi_string:
+    print("Your birthday appears in the first million digits of pi!")
+else:
+    print("You birthday does not appear in the first million digits of pi.")
