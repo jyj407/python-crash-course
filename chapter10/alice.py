ba86263d1785e30d502dc7dc598efbf3539cdc61
Tony <yanjunjiangjn@gmail.com>
2019-04-25 20:46:04 -0400

    [chapter11] more  try except examples.

---
+++ chapter10/alice.py
@@ -0,0 +1,9 @@
+filename = 'alice.txt'
+
+try:
+    with open(filename) as f_obj:
+        contents = f_obj.read()
+except FileNotFoundError:
+    msg = "Sorry, the file " + filename + " does not exist."
+    print(msg)
+
