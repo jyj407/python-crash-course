a7d3a750891f4ab1b915b9f868c79db2e9add69e
Tony <yanjunjiangjn@gmail.com>
2019-04-26 22:03:14 -0400

    [Chapter10] Count multiple books.

--- chapter10/word_count.py
+++ chapter10/word_count.py
@@ -12,5 +12,6 @@ def count_words(filename):
         num_words = len(words)
         print("The file " + filename + " has about " + str(num_words) + " words.")
     
-filename = 'alice.txt'
-count_words(filename)
+filenames = ['alice.txt', 'siddhartha.txt', 'moby_dick.txt', 'little_women.txt']
+for filename in filenames:
+    count_words(filename)
