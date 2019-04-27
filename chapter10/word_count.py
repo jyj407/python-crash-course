eab6ad300b83a7b9743dd59a7e1f6237236f38f7
Tony <yanjunjiangjn@gmail.com>
2019-04-26 21:59:27 -0400

    [Chapter10] abstractd out the count words function

--- chapter10/word_count.py
+++ chapter10/word_count.py
@@ -1,14 +1,16 @@
 filename = 'alice.txt'
-
-try:
-    with open(filename) as f_obj:
-        contents = f_obj.read()
-except FileNotFoundError:
-    msg = "Sorry, the file " +filename + " does not exist."
-    print(msg)
-else:
-    # Count the approximate number of words in the file.
-    words = contents.split()
-    num_words = len(words)
-    print("The file " + filename + " has about " + str(num_words) + " words.")
-
+def count_words(filename):
+    try:
+        with open(filename) as f_obj:
+            contents = f_obj.read()
+    except FileNotFoundError:
+        msg = "Sorry, the file " +filename + " does not exist."
+        print(msg)
+    else:
+        # Count the approximate number of words in the file.
+        words = contents.split()
+        num_words = len(words)
+        print("The file " + filename + " has about " + str(num_words) + " words.")
+    
+filename = 'alice.txt'
+count_words(filename)
