a89faec2301ca50193f6069e575701ff0283d3c9
Tony <yanjunjiangjn@gmail.com>
2019-05-04 20:20:28 -0400

    [Project1] Start firing bullets!!! Nice!

--- project1_alien_invasion/alien_invasion.py
+++ project1_alien_invasion/alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from pygame.sprite import Group
 import game_functions as gf
 
 def run_game():
@@ -11,15 +12,18 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     #Make a ship.
     ship = Ship(ai_settings, screen)
+    # Make a group to store bullets in.
+    bullets = Group()
     
     #pygame.display.set_caption("Alien Invasion")
     pygame.display.set_caption("星球大战")
 
     # Start the main loop for the game.
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        bullets.update()
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 
 run_game()
