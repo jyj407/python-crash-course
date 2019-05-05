77da140239ce96ff03bda7727c4666237996f5e3
Tony <yanjunjiangjn@gmail.com>
2019-05-05 09:17:23 -0400

    [Project1] Fire bullets.

--- project1_alien_invasion/game_functions.py
+++ project1_alien_invasion/game_functions.py
@@ -11,11 +11,15 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
         # MOVE the ship to the left.
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
-        # Create a new bullet and add it to the bullets group.
-        if len(bullets) < ai_settings.bullets_allowed:
-            new_bullet = Bullet(ai_settings, screen, ship)
-            bullets.add(new_bullet)
+        fire_bullet(ai_settings, screen, ship, bullets)
 
+def fire_bullet(ai_settings, screen, ship, bullets):
+    """Fire a new bullet and add it to the bullets group."""
+    # Create a new bullet and add it to the bullets group.
+    if len(bullets) < ai_settings.bullets_allowed:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
+ 
 def check_keyup_events(event, ship):
     """Respond to keypresses."""
     if event.key == pygame.K_RIGHT:
