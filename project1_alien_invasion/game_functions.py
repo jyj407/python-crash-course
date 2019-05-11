cbf2c6ab7d76d2c79e3d11f1d0666dbdb09d4156
Tony <yanjunjiangjn@gmail.com>
2019-05-11 07:47:18 -0400

    [Chapter13] Fix a bug in the previous patch.
    The bug causes the fleet drop immediately aftering reach
    the right edege. Becauses we didn't change the fleet direction
    properly. This is because if the direction is not changed in time,
    all the aliens will reach the right edge and each adds a
    fleet_drop_speed to the alien.rect.y, this quickly adds up, causing
    the fleet dropping down very quickly.
    Also reset the dropping speed to 10 to make is less boring, otherwise
    it is dropping so slow.

--- project1_alien_invasion/game_functions.py
+++ project1_alien_invasion/game_functions.py
@@ -117,7 +117,7 @@ def change_fleet_direction(ai_settings, aliens):
     """Drop the entire fleet and change the fleet's direction."""
     for alien in aliens.sprites():
         alien.rect.y += ai_settings.fleet_drop_speed
-    ai_settings.fleet_direction *= 1
+    ai_settings.fleet_direction *= -1
 
 def update_aliens(ai_settings, aliens):
     """
