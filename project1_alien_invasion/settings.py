d6411e8af41b1e51e5a1f8a7a731cbc1376583e1
Tony <yanjunjiangjn@gmail.com>
2019-05-11 10:06:00 -0400

    Revert "[Chapter13] Making Larger Bullets for Testing."
    This reverts commit b2aba8d9df459d5ac9cefe8eb6ecb1b67d5b4ce4.

--- project1_alien_invasion/settings.py
+++ project1_alien_invasion/settings.py
@@ -16,8 +16,8 @@ class Settings():
 
         # Bullet settings
         self.bullet_speed_factor = 1
-        self.bullet_width = 300
-        self.bullet_height = 150
+        self.bullet_width = 3
+        self.bullet_height = 15
         self.bullet_color = 60, 60, 60
         self.bullets_allowed = 20
 
