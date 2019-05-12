28e0d65d6eeb6186e40315bf13bf3e2aba2d19dc
Tony <yanjunjiangjn@gmail.com>
2019-05-12 10:50:01 -0400

    [Chapter14] Deactivating the Play Button and Hide the Cursor.

--- project1_alien_invasion/game_functions.py
+++ project1_alien_invasion/game_functions.py
@@ -90,7 +90,10 @@ def check_events(ai_settings, screen, stats, play_button, ship, aliens,
 def check_play_button(ai_settings, screen, stats, play_button, ship, aliens,
         bullets, mouse_x, mouse_y):
     """"Start a new game when the player click Play."""
-    if play_button.rect.collidepoint(mouse_x, mouse_y):
+    button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
+    if button_clicked and not stats.game_active:
+        # Hide the mouse cursor.
+        pygame.mouse.set_visible(False)
         # Reset the game statistics.
         stats.reset_stats()
         stats.game_active = True
@@ -189,6 +192,7 @@ def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
         sleep(0.5) # Unit is Second
     else:
         stats.game_active = False
+        pygame.mouse.set_visible(True)
 
     
 def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
