1a9a34dc4cd7553d2c2a4d83feed1af16dacd6d9
Tony <yanjunjiangjn@gmail.com>
2019-05-19 10:46:28 -0400

    [Chapter15] Correcting the Plot.

--- project2_data_visualization/mpl_squares.py
+++ project2_data_visualization/mpl_squares.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt
 
+input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
-plt.plot(squares, linewidth=5)
+
+plt.plot(input_values, squares, linewidth=5)
 
 # Set chart title and label axes.
 plt.title("Square Numbers", fontsize=24)
