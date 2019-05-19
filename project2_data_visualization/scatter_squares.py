1f7134b435fa047612d071494448db90f9036446
Tony <yanjunjiangjn@gmail.com>
2019-05-19 11:10:58 -0400

    [Chapter15] Removing Outlines from Data Points.

--- project2_data_visualization/scatter_squares.py
+++ project2_data_visualization/scatter_squares.py
@@ -1,9 +1,9 @@
 import matplotlib.pyplot as plt
 
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
+x_values = list(range(1, 1001))
+y_values = [x ** 2 for x in x_values]
 
-plt.scatter(x_values, y_values, s=100)
+plt.scatter(x_values, y_values, edgecolor='none', s=40)
 
 # Set chart title and label axes.
 plt.title("Square Numbers", fontsize=24)
@@ -12,5 +12,8 @@ plt.ylabel("Square of Value", fontsize=24)
 
 # Set size of tick labels.
 plt.tick_params(axis='both', which='major', labelsize=14)
+
+# Set the range for each axis.
+plt.axis([0, 1100, 0, 1100000])
 
 plt.show()
