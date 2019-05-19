9f88877bb83a8b4d55ca387fd9c1eb785d0513a4
Tony <yanjunjiangjn@gmail.com>
2019-05-19 10:53:38 -0400

    [Chapter15] Plotting a Series of Ponits with scatter()

--- project2_data_visualization/scatter_squares.py
+++ project2_data_visualization/scatter_squares.py
@@ -1,6 +1,9 @@
 import matplotlib.pyplot as plt
 
-plt.scatter(2, 4, s=200)
+x_values = [1, 2, 3, 4, 5]
+y_values = [1, 4, 9, 16, 25]
+
+plt.scatter(x_values, y_values, s=100)
 
 # Set chart title and label axes.
 plt.title("Square Numbers", fontsize=24)
