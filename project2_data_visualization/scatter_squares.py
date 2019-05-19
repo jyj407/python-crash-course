26025f51d66a81cca93d7d2573c8a7376123b72c
Tony <yanjunjiangjn@gmail.com>
2019-05-19 11:14:39 -0400

    [Chapter16] Using a Colormap.

--- project2_data_visualization/scatter_squares.py
+++ project2_data_visualization/scatter_squares.py
@@ -4,7 +4,8 @@ x_values = list(range(1, 1001))
 y_values = [x ** 2 for x in x_values]
 
 #plt.scatter(x_values, y_values, c='red', edgecolor='none', s=40)
-plt.scatter(x_values, y_values, c=(0, 0, 0.8), edgecolor='none', s=40)
+plt.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues,
+    edgecolor='none', s=40)
 
 # Set chart title and label axes.
 plt.title("Square Numbers", fontsize=24)
