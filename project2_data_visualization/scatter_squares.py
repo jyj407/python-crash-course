b9727cf894b6f251a03db34813939466122a68f8
Tony <yanjunjiangjn@gmail.com>
2019-05-19 11:13:00 -0400

    [Chapter15] Defining Custom Colors.

--- project2_data_visualization/scatter_squares.py
+++ project2_data_visualization/scatter_squares.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 x_values = list(range(1, 1001))
 y_values = [x ** 2 for x in x_values]
 
-plt.scatter(x_values, y_values, edgecolor='none', s=40)
+#plt.scatter(x_values, y_values, c='red', edgecolor='none', s=40)
+plt.scatter(x_values, y_values, c=(0, 0, 0.8), edgecolor='none', s=40)
 
 # Set chart title and label axes.
 plt.title("Square Numbers", fontsize=24)
