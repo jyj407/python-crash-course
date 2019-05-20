97c2322b74d3266097234b3bd93a527c56892927
Tony <yanjunjiangjn@gmail.com>
2019-05-20 10:00:26 -0400

    [Chapter15] Exercise 15-3 Molecular Motion.

---
+++ project2_data_visualization/molecular_motion_15_3.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+from random_walk import RandomWalk
+
+#Keep making new walks, as long as the program is active.
+while True:
+    # Make a random walk, and plot the points.
+    rw = RandomWalk(5000)
+    rw.fill_walk()
+
+    # Set the size of the plotting window.
+    plt.figure(figsize=(10, 6))
+
+    point_numbers = list(range(rw.num_points))
+    plt.plot(rw.x_values, rw.y_values)
+
+    # Remove the axes.
+    plt.axes().get_xaxis().set_visible(False)
+    plt.axes().get_yaxis().set_visible(False)
+
+    plt.show()
+
+    keep_running = input("Make another walk? (y/n)? ")
+    if keep_running == 'n':
+        break
