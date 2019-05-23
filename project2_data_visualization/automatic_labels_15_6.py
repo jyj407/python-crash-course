93b98e5c0457df965c4ab480a8413b8986bc8847
Tony <yanjunjiangjn@gmail.com>
2019-05-22 21:54:03 -0400

    [Chapter15] Exercise 16, using List comprehensions.

--- project2_data_visualization/automatic_labels_15_6.py
+++ project2_data_visualization/automatic_labels_15_6.py
@@ -6,25 +6,17 @@ die_1 = Die()
 die_2 = Die()
 
 # Make some rolls, and store results in a list
-results = []
-for roll_num in range(1000):
-    result = die_1.roll() + die_2.roll()
-    results.append(result)
+results = [ die_1.roll() + die_2.roll() for roll_num in range(1000) ]
 
 # Analyze the results.
-frequencies = []
 max_result = die_1.num_sides + die_2.num_sides
-for value in range(2, max_result + 1):
-    frequency = results.count(value)
-    frequencies.append(frequency)
+frequencies = [ results.count(value) for value in range(2, max_result + 1) ]
 
 # Visualize the results.
 hist = pygal.Bar()
 
-hist.x_labels = []
+hist.x_labels = [ (str(i)) for i in range(2, max_result + 1) ]
 hist.title = "Results of rolling two D6 1000 times."
-for i in range(2, max_result + 1):
-    hist.x_labels.append("'" + str(i) + "'")
 hist.x_title = "Result"
 hist.y_title = "Frequency of Result"
 
