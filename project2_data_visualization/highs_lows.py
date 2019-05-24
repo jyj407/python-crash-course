9e60cb15ee9abba7b9419065925ca5b0141df729
Tony <yanjunjiangjn@gmail.com>
2019-05-23 22:06:13 -0400

    [Chapter16] Plotting the graph with dates.

--- project2_data_visualization/highs_lows.py
+++ project2_data_visualization/highs_lows.py
@@ -1,14 +1,18 @@
 import csv
 from matplotlib import pyplot as plt
+from datetime import datetime
 
-# Get high temperatures from file.
+# Get dates and high temperatures from file.
 filename = 'sitka_weather_07-2014.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
-    highs = []
+    dates, highs = [], []
     for row in reader:
+        current_date = datetime.strptime(row[0], "%Y-%m-%d")
+        dates.append(current_date)
+
         high = int(row[1])
         highs.append(high)
 
@@ -16,11 +20,13 @@ with open(filename) as f:
 
     # Plot data.
     fig = plt.figure(dpi=128, figsize=(10, 6))
-    plt.plot(highs, c='red')
+    plt.plot(dates, highs, c='red')
 
     # Format plot.
     plt.title("Daily high temperatures, July 2014", fontsize=24)
     plt.xlabel('', fontsize=16)
+    # draws the date labels diagonally to prevent them from overlapping
+    fig.autofmt_xdate()
     plt.ylabel('Temperature(F)', fontsize=16)
     plt.tick_params(axis='both', which='major', labelsize=16)
 
