b9fd5813b9081113d20c16854943a4bb728e5192
Tony <yanjunjiangjn@gmail.com>
2019-05-25 10:35:17 -0400

    [Chapter16] Generating countries by Population.

--- project2_data_visualization/world_population.py
+++ project2_data_visualization/world_population.py
@@ -20,8 +20,22 @@ for pop_dict in pop_data:
         else:
             print("ERROR - " + country_name)
 
+cc_pops_1, cc_pops_2, cc_pops_3 = {}, {}, {}
+for cc, pop in cc_populations.items():
+    if pop < 10000000:
+        cc_pops_1[cc] = pop
+    elif ope < 100000000:
+        cc_pops_2[cc] = pop
+    else:
+        cc_pops_d[cc] = pop
+
+# See how many countries are in each level.
+print(len(cc_pops_1), len(cc_pops_2), len(cc_pops_3))
+
 wm = pygal.maps.world.World()
 wm.title = 'World Population in 2010, by Country'
-wm.add('2010', cc_populations)
+wm.add('0-10m', cc_pops_1)
+wm.add('10m-1bn', cc_pops_2)
+wm.add('>1bn', cc_pops_3)
 
 wm.render_to_file('world_population.svg')
