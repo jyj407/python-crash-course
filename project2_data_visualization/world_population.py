b5dc43ca86d8b94110c4af004f12fe0f3fdaa47b
Tony <yanjunjiangjn@gmail.com>
2019-05-25 10:27:45 -0400

    [Chapter16] Plotting a Complete Population Map.

--- project2_data_visualization/world_population.py
+++ project2_data_visualization/world_population.py
@@ -1,11 +1,14 @@
 import json
 from country_codes import get_country_code
+import pygal
 
 # Load the data into a list.
 filename = 'population_data.json'
 with open(filename) as f:
     pop_data = json.load(f)
 
+# Build a dictionary of population data.
+cc_populations = {}
 # Print the 2010 population for each country.
 for pop_dict in pop_data:
     if pop_dict['Year'] == '2010':
@@ -13,6 +16,12 @@ for pop_dict in pop_data:
         population = int(float(pop_dict['Value']))
         code = get_country_code(country_name)
         if code:
-            print(country_name + ": " + str(population))
+            cc_populations[code] = population
         else:
             print("ERROR - " + country_name)
+
+wm = pygal.maps.world.World()
+wm.title = 'World Population in 2010, by Country'
+wm.add('2010', cc_populations)
+
+wm.render_to_file('world_population.svg')
