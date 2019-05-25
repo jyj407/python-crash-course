811d277a957c360fd434d0d8bfcb8a9f2a94e7df
Tony <yanjunjiangjn@gmail.com>
2019-05-24 21:31:06 -0400

    [Chapter16] Shading an Area in the Chart.

--- project2_data_visualization/highs_lows.py
+++ project2_data_visualization/highs_lows.py
@@ -21,9 +21,11 @@ with open(filename) as f:
     print(highs)
 
     # Plot data.
+    # The follwing alpha value controls a color's transparency.
     fig = plt.figure(dpi=128, figsize=(10, 6))
-    plt.plot(dates, highs, c='red')
-    plt.plot(dates, lows, c='blue')
+    plt.plot(dates, highs, c='red', alpha=0.5)
+    plt.plot(dates, lows, c='blue', alpha=0.5)
+    plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
     # Format plot.
     plt.title("Daily high and low temperatures - 2014", fontsize=24)
