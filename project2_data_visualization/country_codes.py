d7d16efba3b23b4e63286e7047c559563f4c815a
Tony <yanjunjiangjn@gmail.com>
2019-05-25 14:55:05 -0400

    [Chapter17] Working with the Response Dictionary.

--- project2_data_visualization/country_codes.py
+++ project2_data_visualization/country_codes.py
@@ -5,11 +5,14 @@ def get_country_code(country_name):
     for code, name in COUNTRIES.items():
         if name == country_name:
             return code
-        
+        elif country_name == 'Yemen, Rep.':
+            return 'ye' 
         # If the country wasn't found, return None.
         return None
 
 print(get_country_code('Andorra'))
 print(get_country_code('Arab World'))
 print(get_country_code('Afghanistan'))
+print(get_country_code('Yemen, Rep.'))
+print(get_country_code('CHINA'))
 
---
+++ project2_data_visualization/python_repos.py
@@ -0,0 +1,32 @@
+import requests
+
+# Make an API call and store the response.
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+r = requests.get(url)
+print("status code:", r.status_code)
+
+# Store API response in a variable
+response_dict = r.json()
+
+print(response_dict.keys())
+print("Total repositories:", response_dict['total_count'])
+
+# Explore information about the repositories.
+repo_dicts = response_dict['items']
+print("Repositories returned:", len(repo_dicts))
+
+# Examine the first repository.
+repo_dict = repo_dicts[0]
+print("\nKeys:", len(repo_dict))
+for key in sorted(repo_dict.keys()):
+    print(key)
+
+print("\nSelected information about first repository:")
+print('Name:', repo_dict['name'])
+print('Owner:', repo_dict['owner']['login'])
+print('Stars:', repo_dict['stargazers_count'])
+print('Repository:', repo_dict['html_url'])
+print('Created:', repo_dict['created_at'])
+print('Updated:', repo_dict['updated_at'])
+print('Descriptioin:', repo_dict['description'])
+
