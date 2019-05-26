b5dbb74ea5491c3ef1463570f5fd9c5ed3a1d184
Tony <yanjunjiangjn@gmail.com>
2019-05-26 08:12:08 -0400

    [Chapter17] Print every repository.

--- project2_data_visualization/python_repos.py
+++ project2_data_visualization/python_repos.py
@@ -15,18 +15,13 @@ print("Total repositories:", response_dict['total_count'])
 repo_dicts = response_dict['items']
 print("Repositories returned:", len(repo_dicts))
 
-# Examine the first repository.
-repo_dict = repo_dicts[0]
-print("\nKeys:", len(repo_dict))
-for key in sorted(repo_dict.keys()):
-    print(key)
-
-print("\nSelected information about first repository:")
-print('Name:', repo_dict['name'])
-print('Owner:', repo_dict['owner']['login'])
-print('Stars:', repo_dict['stargazers_count'])
-print('Repository:', repo_dict['html_url'])
-print('Created:', repo_dict['created_at'])
-print('Updated:', repo_dict['updated_at'])
-print('Descriptioin:', repo_dict['description'])
-
+# Examine every repository.
+for repo_dict in repo_dicts:
+    print("\nSelected information about first repository:")
+    print('Name:', repo_dict['name'])
+    print('Owner:', repo_dict['owner']['login'])
+    print('Stars:', repo_dict['stargazers_count'])
+    print('Repository:', repo_dict['html_url'])
+    print('Created:', repo_dict['created_at'])
+    #print('Updated:', repo_dict['updated_at'])
+    print('Descriptioin:', repo_dict['description'])
